c51f659e78de8c406dc6d070d0b3aeaf66849d63
henZ <65068770+henZ1010@users.noreply.github.com>
2022-02-18 23:56:54 +0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "index.html"
+  ],
+  theme: {
+    screens: {
+      'sm': '350px',
+      'md': '768px',
+      'smdesktop': '1360px',
+      'halfdesktop': '678px',
+      'laptop': '1536px',
+      'halflaptop': '767px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
