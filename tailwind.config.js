e119751bb23425e494e635fbddc8768a032763a3
henZ <65068770+henZ1010@users.noreply.github.com>
2022-03-05 01:51:44 +0800

    Install postcss and linting tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,19 @@
 module.exports = {
-  content: [
-    "./src/**/*.{html,js}",
-    "index.html"
-  ],
+  content: ["./src/**/*.{html,js}", "index.html"],
   theme: {
     screens: {
-      'sm': '350px',
-      'md': '768px',
-      'smdesktop': '1360px',
-      'halfdesktop': '678px',
-      'laptop': '1536px',
-      'halflaptop': '767px',
+      sm: "350px",
+      md: "768px",
+      smdesktop: "1360px",
+      halfdesktop: "678px",
+      laptop: "1536px",
+      halflaptop: "767px",
     },
     extend: {
       fontFamily: {
-        'sans': ['Rubik Regular', 'sans-serif'],
+        sans: ["Rubik Regular", "sans-serif"],
       },
     },
   },
   plugins: [],
-}
+};
